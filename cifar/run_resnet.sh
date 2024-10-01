64452419d58ce6998203fcaeae03d4022d30a225
viki <wuyanru_1998@outlook.com>
2024-10-02 05:54:29 +0800

    change argument

--- cifar/run_resnet.sh
+++ cifar/run_resnet.sh
@@ -6,4 +6,4 @@ current_date=$(date +%Y-%m-%d)
 # Create a directory with the current date
 mkdir -p result_$current_date
 
-python train_resnet.py --use_adam --custom_network_init --plateau_lr_scheduler --lambda_lr_scheduler --epochs 1 --emb_reg --emb_metric Hembedding --temb_size 24 --lr 0.0005 --emb_beta 0.05 --random_seed 42
+python train_resnet.py --use_adam --custom_network_init --plateau_lr_scheduler --lambda_lr_scheduler --epochs 100 --emb_reg --emb_metric Hembedding --temb_size 32 --lr 0.0005 --emb_beta 0.05 --random_seed 42
